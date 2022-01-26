0594f017cc741ac4ae044cedb5d47232197f141c
BOSSA Mickael <mickael.bossa@outlook.com>
2022-01-26 18:58:55 +0100

    Add function checkValidity of the parameter screen

--- screens/Parameters.js
+++ screens/Parameters.js
@@ -1,5 +1,5 @@
 // Librairies
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -7,33 +7,73 @@ import {
   Dimensions,
   TextInput,
   TouchableOpacity,
+  Keyboard,
+  Alert,
 } from 'react-native';
 import Colors from '../constants/Colors';
 
 export default function Parameters() {
+  // States
+  const [minPrice, setMinPrice] = useState('0');
+  const [maxPrice, setMaxPrice] = useState('1000');
+  const [errorInput, setErrorInput] = useState(false)
+
+  // Fonctions
+  const checkValidity = (e, input) => {
+    
+    // Vérifier que les valeurs sont corrects
+    if(isNaN(e)) {
+      setErrorInput(true)
+    } else {
+      setErrorInput(false);
+      setMinPrice(e)
+    }
+  };
+
+  const onSubmitPressedHandler = () => {
+    // Fermer le clavier
+    Keyboard.dismiss();
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Paramètres</Text>
       <View style={styles.parametersCard}>
         <View style={styles.form}>
-        <Text style={styles.txt}>Prix minimum</Text>
-        <TextInput
-          style={styles.input}
-          placeholder='0'
-          placeholderTextColor={'black'}
-        />
+          <Text style={styles.txt}>Prix minimum</Text>
+          <TextInput
+            style={errorInput ? styles.errorInput : styles.input}
+            placeholder={minPrice}
+            placeholderTextColor={'black'}
+            onChangeText={(e, input) => checkValidity(e, setMinPrice)}
+            value={minPrice}
+            keyboardType='number-pad'
+          />
+          <Text style={styles.errorMsg}>
+            {errorInput ? "Ceci n'est pas un nombre" : ''}
+          </Text>
         </View>
         <View style={styles.form}>
-        <Text style={styles.txt}>Prix maximum</Text>
-        <TextInput
-          style={styles.input}
-          placeholder='1000'
-          placeholderTextColor={'black'}
-        />
+          <Text style={styles.txt}>Prix maximum</Text>
+          <TextInput
+            style={errorInput ? styles.errorInput : styles.input}
+            placeholder={maxPrice}
+            placeholderTextColor={'black'}
+            onChangeText={checkValidity}
+            value={maxPrice}
+            keyboardType='number-pad'
+          />
+          <Text style={styles.errorMsg}>
+            {errorInput ? "Ceci n'est pas un nombre" : ''}
+          </Text>
+        </View>
       </View>
-      </View>
-      <TouchableOpacity style={styles.btn}>
-        <Text style={{color: '#fff'}}>Sauvegarder</Text>
+      <TouchableOpacity
+        style={styles.btn}
+        activeOpacity={0.8}
+        onPress={onSubmitPressedHandler}
+      >
+        <Text style={{ color: '#fff' }}>Sauvegarder</Text>
       </TouchableOpacity>
     </View>
   );
@@ -57,7 +97,7 @@ const styles = StyleSheet.create({
     marginBottom: Dimensions.get('window').height * 0.025,
     justifyContent: 'space-around',
     alignItems: 'center',
-    borderRadius: Dimensions.get('window').width * 0.05
+    borderRadius: Dimensions.get('window').width * 0.05,
   },
   form: {
     width: '100%',
@@ -71,6 +111,17 @@ const styles = StyleSheet.create({
     height: '17%',
     borderRadius: 3,
   },
+  errorInput: {
+    backgroundColor: '#fff',
+    width: '85%',
+    height: '17%',
+    borderRadius: 3,
+    backgroundColor: '#F45A5F',
+  },
+  errorMsg: {
+    color: '#F45A5F',
+    marginTop: 1,
+  },
   txt: {
     width: '85%',
     marginBottom: Dimensions.get('window').height * 0.01,
@@ -81,5 +132,5 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.primary,
     borderRadius: Dimensions.get('window').width * 0.015,
     marginTop: Dimensions.get('window').height * 0.05,
-  }
+  },
 });
